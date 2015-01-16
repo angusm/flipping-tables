83fe118f49b40ebbd7f317181413973ae260702f
Angus MacDonald <angus@angusmacdonald.ca>
2015-01-15 22:00:26 -0800

    Started adding column filters, fixed dumb classes.
    Next up, loading data from the element. Yeah, that should probably be
    next.

--- flipping-tables.js
+++ flipping-tables.js
@@ -37,6 +37,8 @@ flippingTables.controller(
 flippingTables.factory('column', function() {
 	return function(propertyName) {
 
+		//PROPERTIES
+
 		// The property name, the value to use to retrieve the information
 		// from the entries
 		this.propertyName = propertyName;
@@ -45,14 +47,76 @@ flippingTables.factory('column', function() {
 		this.label = propertyName;
 
 		// Store the conversion information
-		this.displayFilters = {};
+		this.displayFilters = [];
+
+		// FUNCTIONS
+
+		/**
+		 * Convert the value to a display
+		 * @param value
+		 * @returns {*}
+		 */
+		this.displayValue = function(value) {
+
+			// Change the value through the filters and return the result
+			$.each(
+				this.displayFilters,
+				function(index,filter) {
+					value = filter.convert(value);
+				}
+			);
+			return value;
+
+		};
 
 	};
 });
 
-// Column Filter Factory
-flippingTables.factory('column', function() {
-	return function() {
+// Column Filter Factories
+// ------------------------------------------------------------------------------------------------------------
 
+// Generic filter, does nothing, designed to be extended
+flippingTables.factory('ColumnFilter', function() {
+	return function() {
+		this.convert = function(data) {
+			return data;
+		};
 	};
+});
+
+// Currency filter factory
+flippingTables.factory('currencyFilter', function() {
+
+	// Use the generic column filter and adjust it
+	var currencyFilter  = new DecimalFilterFactory(2);
+	currencyFilter.convert = function(data) {
+		var conversion = new DecimalFilterFactory(conversion.decimalPlaces).convert(data);
+		conversion = '$' + conversion;
+		return conversion;
+	};
+	return currencyFilter;
+});
+
+// Decimal filter factory
+flippingTables.factory('decimalFilter', function(decimalPlaces) {
+
+	// Use the generic column filter and adjust it
+	var decimalFilter  = new ColumnFilterFactory();
+
+	// Add the decimal places
+	decimalFilter.prototype.decimalPlaces = decimalPlaces;
+
+	// Adjust the convert function
+	decimalFilter.convert = function(data) {
+		var conversion = new ColumnFilterFactory().convert(data);
+		if ($.isNumeric(data)) {
+			conversion = parseFloat(conversion);
+			conversion = conversion.toFixed(this.decimalPlaces);
+		}
+		return conversion;
+	};
+
+	// Return the result
+	return decimalFilter;
+
 });
